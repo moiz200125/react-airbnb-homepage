f9fc22038f4a85e2f5917b4b2c766a0e58d15b07
moiz200125 <130084727+moiz200125@users.noreply.github.com>
2024-11-17 15:29:07 +0500

    Added the functionality of Search Bar

--- airbnb-app/src/App.jsx
+++ airbnb-app/src/App.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import SearchBar from './components/SearchBar';
 import './App.css'; 
+import Counter from './Conter';
 import Footer from './components/Footer';
 
 function App() {
-  return (
+  return 
+  (
     <div className="App">
       <Navbar />
       <SearchBar />
+      <Counter/>
       <Footer/>
     </div>
   );
 }
 
 export default App;
-     
--- airbnb-app/src/index.js
+++ airbnb-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App1';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
